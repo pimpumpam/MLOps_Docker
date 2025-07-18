75f64f8b98e495a284e5077e3733d7c6cd007f56
백창선 <pimpumpam@baegchangseon-ui-MacBookPro.local>
2025-07-18 11:02:24 +0900

    Train Workflow v0.2.5 - 모델 학습을 위한 유틸 기능 추가

--- crypto_forecast/utils/utils.py
+++ crypto_forecast/utils/utils.py
@@ -1,5 +1,8 @@
+import mlflow
+from itertools import product
 from typing import Sequence, cast
 
+PROGRESS_BAR_FORMAT = '{l_bar}{bar:10}{r_bar}'
 
 def load_spec_from_base_config(cfg_name):
 
@@ -38,6 +41,7 @@ def load_spec_from_base_config(cfg_name):
     return meta_spec, load_spec, preprocess_spec, transform_spec, train_spec, hyperparameter_spec, evaluate_spec, deploy_spec
 
 
+
 def read_sql_file(filepath):
     """
     SQL 폴더 내 쿼리 읽어오기
@@ -54,3 +58,48 @@ def read_sql_file(filepath):
         query = f.read()
         
     return query
+
+
+def setup_experiment(experiment_name, artifact_location):
+    """
+    MLFlow 기반 Experiment 설정. 존재하지 않는 경우 새로운 Experiment 생성. 존재하는 경우 해당 Experiment 내 기록.
+
+    Args:
+        experiment_name (str): Experiment 명
+        artifact_location (str): Artiface 저장 경로
+    """
+    
+    try:
+        mlflow.create_experiment(
+            experiment_name, 
+            artifact_location=artifact_location
+        )
+        print(
+            f"🧪 Experiment {experiment_name} is not Exist. \
+            Create Experiment."
+        )
+    except:
+        print(
+            f"🧪 Experienmt {experiment_name} is Already Exist. \
+            Execute Run on the \"{experiment_name}\"."
+        )
+            
+    # set experiment
+    mlflow.set_experiment(experiment_name)
+    
+
+def hyperparameter_combination(cfg_hyp):
+    """
+    하이퍼파라미터의 모든 조합 산출
+
+    Args:
+        cfg_hyp (class): Config 파일에 정의 된 하이퍼파라미터 정보
+
+    Returns:
+        combinations (list): {하이퍼파라미터 명: 하이퍼파라미터 값, ... } 형태의 딕셔너리들로 구성 된 리스트
+    """
+    attributes = {attr: getattr(cfg_hyp, attr) for attr in dir(cfg_hyp) if not attr.startswith("__") and not callable(getattr(cfg_hyp, attr))}
+    hyps, vals = attributes.keys(), attributes.values()
+    combinations = [dict(zip(hyps, comb)) for comb in product(*vals)]
+    
+    return combinations
